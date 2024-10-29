6d836e514ddb08c0685a1fda537e5e726a3ddaee
Crois0509 <crois0509@naver.com>
2024-10-29 13:55:22 +0900

    [feat] Lv3 구현

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -2,8 +2,22 @@ import UIKit
 
 // Lv.1 Calculator 클래스 생성하기
 class Calculator {
+    // Lv.3 Calculator 코드 수정하기
+    // 프로퍼티 선언
+    var firstNumber: Int
+    var secondNumber: Int
+    
+    // Lv.3 Calculator 코드 수정하기
+    // 이니셜라이저 추가
+    init(firstNumber: Int, secondNumber: Int) {
+        self.firstNumber = firstNumber
+        self.secondNumber = secondNumber
+    }
+    
     // Lv.1 연산기능 구현하기
-    func calculate(operatorSymbol: String, firstNumber: Int, secondNumber: Int) -> Int {
+    // Lv.3 Calculator 코드 수정하기
+    // 매개변수 제거
+    func calculate(operatorSymbol: String) -> Int {
         var result: Int = 0
         
         // 더하기 구현
@@ -32,13 +46,81 @@ class Calculator {
 }
 
 // Lv.1 인스턴스화 후 연산하여 출력하기
-let calculator = Calculator()
+//let calculator = Calculator()
 
-print(calculator.calculate(operatorSymbol: "+", firstNumber: 5, secondNumber: 5)) // 10
-print(calculator.calculate(operatorSymbol: "-", firstNumber: 5, secondNumber: 5)) // 0
-print(calculator.calculate(operatorSymbol: "*", firstNumber: 5, secondNumber: 5)) // 25
-print(calculator.calculate(operatorSymbol: "/", firstNumber: 5, secondNumber: 5)) // 1
-
+//print(calculator.calculate(operatorSymbol: "+", firstNumber: 5, secondNumber: 5)) // 10
+//print(calculator.calculate(operatorSymbol: "-", firstNumber: 5, secondNumber: 5)) // 0
+//print(calculator.calculate(operatorSymbol: "*", firstNumber: 5, secondNumber: 5)) // 25
+//print(calculator.calculate(operatorSymbol: "/", firstNumber: 5, secondNumber: 5)) // 1
+//
 // Lv.2 나머지 연산 출력&예외상황 출력
-print(calculator.calculate(operatorSymbol: "%", firstNumber: 5, secondNumber: 5)) // 0
-print(calculator.calculate(operatorSymbol: " ", firstNumber: 5, secondNumber: 5)) // 오류
+//print(calculator.calculate(operatorSymbol: "%", firstNumber: 5, secondNumber: 5)) // 0
+//print(calculator.calculate(operatorSymbol: " ", firstNumber: 5, secondNumber: 5)) // 오류
+
+// Lv.3 연산자 클래스 생성하기
+// 더하기 연산자 클래스 생성
+class AddOperation: Calculator {
+    init(first: Int, second: Int) {
+        super.init(firstNumber: first, secondNumber: second)
+    }
+    
+    override func calculate(operatorSymbol: String = "+") -> Int {
+        super.calculate(operatorSymbol: operatorSymbol)
+    }
+}
+
+// 빼기 연산자 클래스 생성
+class SubstractOperation: Calculator {
+    init(first: Int, second: Int) {
+        super.init(firstNumber: first, secondNumber: second)
+    }
+    
+    override func calculate(operatorSymbol: String = "-") -> Int {
+        super.calculate(operatorSymbol: operatorSymbol)
+    }
+}
+
+// 곱하기 연산자 클래스 생성
+class MultiplyOperation: Calculator {
+    init(first: Int, second: Int) {
+        super.init(firstNumber: first, secondNumber: second)
+    }
+    
+    override func calculate(operatorSymbol: String = "*") -> Int {
+        super.calculate(operatorSymbol: operatorSymbol)
+    }
+}
+
+// 나누기 연산자 클래스 생성
+class DivideOperation: Calculator {
+    init(first: Int, second: Int) {
+        super.init(firstNumber: first, secondNumber: second)
+    }
+    
+    override func calculate(operatorSymbol: String = "/") -> Int {
+        super.calculate(operatorSymbol: operatorSymbol)
+    }
+}
+
+// 나머지 연산자 클래스 생성
+class RemainingOperation: Calculator {
+    init(first: Int, second: Int) {
+        super.init(firstNumber: first, secondNumber: second)
+    }
+    
+    override func calculate(operatorSymbol: String = "%") -> Int {
+        super.calculate(operatorSymbol: operatorSymbol)
+    }
+}
+
+let addOperator = AddOperation(first: 10, second: 20) // 30
+let substractOperation = SubstractOperation(first: 100, second: 50) // 50
+let multiplyOperation = MultiplyOperation(first: 2, second: 9) // 18
+let divideOperation = DivideOperation(first: 10, second: 2) // 5
+let remainingOperation = RemainingOperation(first: 10, second: 4) // 2
+
+print(addOperator.calculate())
+print(substractOperation.calculate())
+print(multiplyOperation.calculate())
+print(divideOperation.calculate())
+print(remainingOperation.calculate())
--- README.md
+++ README.md
@@ -22,11 +22,14 @@
     - AdstractOperation라는 **추상화**된 프로토콜 만들기
     - 기존에 구현한 AddOperation(더하기), SubstractOperation(빼기), MultiplyOperation(곱하기), DivideOperation(나누기) 클래스들과 관계를 맺고 Calculator 클래스의 내부 코드를 변경
     - 스위프트의 어떤 문법을 이용하여 추상화할 수 있을지 생각해 봅시다
-###
+### 
 - **Lv.2 진행 중 에외처리 상황에 대한 고민**
     - Lv.1의 상태에서 calculate 메서드를 사용했을 때, 매개변수인 operatorSymbol 값으로 지정되지 않은 연산자(공백값이나 기타 다른 String 값)가 사용될 경우에 대한 대책이 없다.
     - 때문에 이를 대처할 수 있도록 if문에 else 라는 경우의 수를 만들고, 지정되지 않은 연산자가 사용되었을 때 사용자에게 경고를 보내는 코드를 추가하였다.
 
 - **Lv.3 작성 완료 후 Lv.2 와 비교하여 개선된 점(hint.클래스의 책임(단일책임원칙))**
+    - 기존 Calculator 클래스의 메소드인 calculate를 사용하기 위해서는 Calculator의 인스턴스를 만들고, calculate 메소드를 호출하여 사용할 수 있었다. 이 때, 매개변수로 operatorSymbol 및 계산할 숫자를 모두 입력해서 사용해야 하는 불편함이 있었다.
+    - Calculate의 자식클래스로 연산자를 나누어 클래스를 생성하며 Calculator에 프로퍼티와 init 을 추가하고, 이를 자식클래스에서 초기화할 수 있도록 하였다. 또, calculate 메소드를 재정의 하여 각 연산자 클래스별로 operatorSymbol을 갖게 하고, 이를 통해 별도의 매개변수 입력 없이도 연산을 가능하게 하였다.
+    - 이를 통해 본래 Calculate의 메소드인 calculate를 사용할 때 발생할 수 있었던 예외상황(잘못된 연산자 입력 등)을 방지하고, Calculate에서 할 일을 나누어 가짐으로써 불필요한 코드의 사용을 줄일 수 있었다.
 
 - **Lv.4 작성 완료 후 Lv.3 와 비교하여 개선된 점(hint.클래스간의 결합도, 의존성(의존성역전원칙))**
