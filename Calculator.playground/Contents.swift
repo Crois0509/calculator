bed1b4adca5c9d06aedd79482f20af4b47b0c6e6
Crois0509 <crois0509@naver.com>
2024-10-29 11:50:13 +0900

    [feat] Lv1 구현
    ## 1. 추가기능
    - Calculator 클래스 추가
    - calculate 메서드 추가

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,3 +1,32 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Lv.1 Calculator 클래스 생성하기
+class Calculator {
+    // Lv.1 연산기능 구현하기
+    func calculate(operatorSymbol: String, firstNumber: Int, secondNumber: Int) -> Int {
+        var result: Int = 0
+        
+        // 더하기 구현
+        if operatorSymbol == "+" {
+            result = firstNumber + secondNumber
+        // 빼기 구현
+        } else if operatorSymbol == "-" {
+            result = firstNumber - secondNumber
+        // 곱하기 구현
+        } else if operatorSymbol == "*" {
+            result = firstNumber * secondNumber
+        // 나누기 구현
+        } else if operatorSymbol == "/" {
+            result = firstNumber / secondNumber
+        }
+        
+        return result
+    }
+}
+
+// Lv.1 인스턴스화 후 연산하여 출력하기
+let calculator = Calculator()
+print(calculator.calculate(operatorSymbol: "+", firstNumber: 5, secondNumber: 5)) // 10
+print(calculator.calculate(operatorSymbol: "-", firstNumber: 5, secondNumber: 5)) // 0
+print(calculator.calculate(operatorSymbol: "*", firstNumber: 5, secondNumber: 5)) // 25
+print(calculator.calculate(operatorSymbol: "/", firstNumber: 5, secondNumber: 5)) // 1
--- README.md
+++ README.md
@@ -23,6 +23,7 @@
     - AdstractOperation라는 **추상화**된 프로토콜 만들기
     - 기존에 구현한 AddOperation(더하기), SubstractOperation(빼기), MultiplyOperation(곱하기), DivideOperation(나누기) 클래스들과 관계를 맺고 Calculator 클래스의 내부 코드를 변경
     - 스위프트의 어떤 문법을 이용하여 추상화할 수 있을지 생각해 봅시다
+    
 ###
 - Lv.2 진행 중 에외처리 상황에 대한 고민
 
